28857d1e2220ace5b073b23523d86bd608a6cfb8
fernandobenitez33 <Nanbeto.05@gmail.com>
2024-04-18 14:49:48 +0200

    changes

--- src/main/java/CentroEducativo/controladores/SuperControlador.java
+++ src/main/java/CentroEducativo/controladores/SuperControlador.java
@@ -1,13 +1,11 @@
 package CentroEducativo.controladores;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 
 import CentroEducativo.entidades.Entidad;
 import CentroEducativo.entidades.Estudiante;
@@ -26,21 +24,10 @@ public class SuperControlador {
 		this.tipoEntidad = tipoEntidad;
 	}
 
-	private static void update(Entidad e) {
-		EntityManager em = getEntityManager();
-		em.getTransaction().begin();
-		em.merge(e);
-		em.getTransaction().commit();
-	}
-
-	public List<? extends Entidad> findAll() {
-		return (List<Entidad>) getEntityManager()
-				.createNativeQuery("SELECT * FROM " + nombreTabla + ";", this.tipoEntidad).getResultList();
-
-	}
-	
-	
-
+	/**
+	 * 
+	 * @return
+	 */
 	protected static EntityManager getEntityManager() {
 		if (em == null) {
 			em = Persistence.createEntityManagerFactory("Centro Educativo").createEntityManager();
@@ -48,57 +35,96 @@ public class SuperControlador {
 		return em;
 	}
 
-	public static ValoracionMateria obtenerValoracion(Estudiante e, Profesor p, Materia m, Integer nota) {
-		try {
-			return (ValoracionMateria) getEntityManager().createNativeQuery(
-					"SELECT * FROM valoracionmateria where " + e.getId() + " = idEstudiante and " + p.getId()
-							+ " = idProfesor and " + m.getId() + " = idMateria and " + nota + " = valoracion;",
-					ValoracionMateria.class).getSingleResult();
-		}
-		catch (NoResultException ex) {
-			return null;
-		}
-		
-	}
-	
-	
-	public static void insert(Estudiante e, Profesor p, Materia m, Integer nota) {
-		ValoracionMateria v = new ValoracionMateria();
-		v.setIdEstudiante(e.getId());
-		v.setIdMateria(m.getId());
-		v.setIdProfesor(p.getId());
-		v.setValoracion(nota);
-		
-		
-		em.getTransaction().begin();
-		em.persist(v);
-		em.getTransaction().commit();
-		
-	}
-	
-	
-	public static void update(Estudiante e, Profesor p, Materia m, Integer nota) {
-		ValoracionMateria v = obtenerValoracionSinNota(e, p, m);
-		
-		v.setValoracion(nota);
-		
-		em.getTransaction().begin();
-		em.persist(v);
-		em.getTransaction().commit();
-		
-	}
-	
-	public static ValoracionMateria obtenerValoracionSinNota(Estudiante e, Profesor p, Materia m) {
-		try {
-			return (ValoracionMateria) getEntityManager().createNativeQuery(
-					"SELECT * FROM valoracionmateria where " + e.getId() + " = idEstudiante and " + p.getId()
-							+ " = idProfesor and " + m.getId() + " = idMateria;",
-					ValoracionMateria.class).getSingleResult();
-		}
-		catch (NoResultException ex) {
-			return null;
-		}
-		
+	/**
+	 * 
+	 * @return
+	 */
+	public List<? extends Entidad> findAll() {
+		return (List<Entidad>) getEntityManager()
+				.createNativeQuery("SELECT * FROM " + nombreTabla + ";", this.tipoEntidad).getResultList();
+
 	}
 
+	/**
+	 * 
+	 * @param estudiante
+	 * @param profesor
+	 * @param materia
+	 * @return
+	 */
+	public static ValoracionMateria obtenerNota(Estudiante estudiante, Profesor profesor, Materia materia) {
+		try {
+			return (ValoracionMateria) getEntityManager().createNativeQuery(
+					"SELECT * FROM valoracionmateria where " + estudiante.getId() + " = idEstudiante and " + profesor.getId()
+							+ " = idProfesor and " + materia.getId() + " = idMateria;",
+					ValoracionMateria.class).getSingleResult();
+		}
+		catch (NoResultException ex) {
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param estudiante
+	 * @param profesor
+	 * @param materia
+	 * @param nota
+	 * @return
+	 */
+	public static ValoracionMateria ordenarEstudiantes(Estudiante estudiante, Profesor profesor, Materia materia, Integer nota) {
+		try {
+			return (ValoracionMateria) getEntityManager().createNativeQuery(
+					"SELECT * FROM valoracionmateria where " + estudiante.getId() + " = idEstudiante and " + profesor.getId()
+							+ " = idProfesor and " + materia.getId() + " = idMateria and " + nota + " = valoracion;",
+					ValoracionMateria.class).getSingleResult();
+		}
+		catch (NoResultException ex) {
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param estudiante
+	 * @param profesor
+	 * @param materia
+	 * @param nota
+	 * @param fecha
+	 */
+	public static void insert(Estudiante estudiante, Profesor profesor, Materia materia, Integer nota, Date fecha) {
+		ValoracionMateria v = new ValoracionMateria();
+		v.setIdEstudiante(estudiante.getId());
+		v.setIdMateria(materia.getId());
+		v.setIdProfesor(profesor.getId());
+		v.setValoracion(nota);
+		v.setFecha(fecha);
+		
+		em.getTransaction().begin();
+		em.persist(v);
+		em.getTransaction().commit();
+		
+	}
+	
+	/**
+	 * 
+	 * @param estudiante
+	 * @param profesor
+	 * @param materia
+	 * @param nota
+	 * @param fecha
+	 */
+	public static void update(Estudiante estudiante, Profesor profesor, Materia materia, Integer nota, Date fecha) {
+		ValoracionMateria v = obtenerNota(estudiante, profesor, materia);
+		
+		v.setValoracion(nota);
+		v.setFecha(fecha);
+		
+		em.getTransaction().begin();
+		em.persist(v);
+		em.getTransaction().commit();
+		
+	}
 }
--- src/main/java/CentroEducativo/vistas/PanelValoracionMateria.java
+++ src/main/java/CentroEducativo/vistas/PanelValoracionMateria.java
@@ -1,12 +1,16 @@
 package CentroEducativo.vistas;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import CentroEducativo.controladores.ControladorEstudiante;
 import CentroEducativo.controladores.ControladorMateria;
 import CentroEducativo.controladores.ControladorProfesor;
+import CentroEducativo.controladores.ControladorValoracionMateria;
+import CentroEducativo.controladores.SuperControlador;
 import CentroEducativo.entidades.Estudiante;
 import CentroEducativo.entidades.Materia;
 import CentroEducativo.entidades.Profesor;
@@ -16,9 +20,16 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.Color;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.awt.event.ActionEvent;
+import javax.swing.JFormattedTextField;
 
 public class PanelValoracionMateria extends JPanel {
 
@@ -26,8 +37,12 @@ public class PanelValoracionMateria extends JPanel {
 	JComboBox<Materia> comboBoxMateria;
 	JComboBox<Profesor> comboBoxProfesor;
 	JComboBox<Integer> comboBoxNota;
-	JList<Estudiante> listNoSeleccionado;
-	JList<Estudiante> listSeleccionado;
+	JList<Estudiante> listNoSeleccionados;
+	JList<Estudiante> listSeleccionados;
+	private DefaultListModel<Estudiante> listMNoSeleccionados;
+	private DefaultListModel<Estudiante> listMSeleccionados;
+	private JFormattedTextField jftf;
+	Date fecha;
 
 	/**
 	 * Create the panel.
@@ -50,9 +65,9 @@ public class PanelValoracionMateria extends JPanel {
 		add(panelArriba, gbc_panelArriba);
 		GridBagLayout gbl_panelArriba = new GridBagLayout();
 		gbl_panelArriba.columnWidths = new int[] { 0, 0, 0 };
-		gbl_panelArriba.rowHeights = new int[] { 0, 0, 0, 0, 0 };
+		gbl_panelArriba.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
 		gbl_panelArriba.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
-		gbl_panelArriba.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panelArriba.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		panelArriba.setLayout(gbl_panelArriba);
 
 		JLabel lblMateria = new JLabel("Materia:");
@@ -105,10 +120,31 @@ public class PanelValoracionMateria extends JPanel {
 		panelArriba.add(comboBoxNota, gbc_comboBoxNota);
 
 		JButton btnBotonDeActualizado = new JButton("Botón actualizar alumno");
+		btnBotonDeActualizado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				actualizarAlumnado();
+			}
+		});
+
+		JLabel lblFecha = new JLabel("Fecha:");
+		GridBagConstraints gbc_lblFecha = new GridBagConstraints();
+		gbc_lblFecha.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFecha.anchor = GridBagConstraints.EAST;
+		gbc_lblFecha.gridx = 0;
+		gbc_lblFecha.gridy = 3;
+		panelArriba.add(lblFecha, gbc_lblFecha);
+
+		JFormattedTextField TextFieldFecha = new JFormattedTextField();
+		GridBagConstraints gbc_TextFieldFecha = new GridBagConstraints();
+		gbc_TextFieldFecha.insets = new Insets(0, 0, 5, 0);
+		gbc_TextFieldFecha.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TextFieldFecha.gridx = 1;
+		gbc_TextFieldFecha.gridy = 3;
+		panelArriba.add(getJFormattedTextFieldDatePersonalizado(), gbc_TextFieldFecha);
 		GridBagConstraints gbc_btnBotonDeActualizado = new GridBagConstraints();
 		gbc_btnBotonDeActualizado.anchor = GridBagConstraints.EAST;
 		gbc_btnBotonDeActualizado.gridx = 1;
-		gbc_btnBotonDeActualizado.gridy = 3;
+		gbc_btnBotonDeActualizado.gridy = 4;
 		panelArriba.add(btnBotonDeActualizado, gbc_btnBotonDeActualizado);
 
 		JPanel panelCentro = new JPanel();
@@ -142,13 +178,13 @@ public class PanelValoracionMateria extends JPanel {
 		gbc_lblAlumnadoSeleccionado.gridy = 0;
 		panelCentro.add(lblAlumnadoSeleccionado, gbc_lblAlumnadoSeleccionado);
 
-		listNoSeleccionado = new JList();
+		listNoSeleccionados = new JList(this.getListModelNOSeleccionados());
 		GridBagConstraints gbc_listNoSeleccionado = new GridBagConstraints();
 		gbc_listNoSeleccionado.insets = new Insets(0, 0, 0, 5);
 		gbc_listNoSeleccionado.fill = GridBagConstraints.BOTH;
 		gbc_listNoSeleccionado.gridx = 0;
 		gbc_listNoSeleccionado.gridy = 1;
-		panelCentro.add(listNoSeleccionado, gbc_listNoSeleccionado);
+		panelCentro.add(listNoSeleccionados, gbc_listNoSeleccionado);
 
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 190, 111));
@@ -169,18 +205,21 @@ public class PanelValoracionMateria extends JPanel {
 		JButton button_2 = new JButton(">");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				pasarASeleccionado();
 			}
 		});
 
 		JButton button_1 = new JButton("<");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				quitarDeSeleccionado();
 			}
 		});
 
 		JButton button = new JButton("<<");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				quitarTodosDeSeleccionados();
 			}
 		});
 		GridBagConstraints gbc_button = new GridBagConstraints();
@@ -200,18 +239,23 @@ public class PanelValoracionMateria extends JPanel {
 		panel.add(button_2, gbc_button_2);
 
 		JButton button_3 = new JButton(">>");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pasarTodosASeleccionados();
+			}
+		});
 		GridBagConstraints gbc_button_3 = new GridBagConstraints();
 		gbc_button_3.insets = new Insets(0, 0, 5, 5);
 		gbc_button_3.gridx = 1;
 		gbc_button_3.gridy = 4;
 		panel.add(button_3, gbc_button_3);
 
-		listSeleccionado = new JList();
+		listSeleccionados = new JList(this.getListModelSeleccionados());
 		GridBagConstraints gbc_listSeleccionado = new GridBagConstraints();
 		gbc_listSeleccionado.fill = GridBagConstraints.BOTH;
 		gbc_listSeleccionado.gridx = 6;
 		gbc_listSeleccionado.gridy = 1;
-		panelCentro.add(listSeleccionado, gbc_listSeleccionado);
+		panelCentro.add(listSeleccionados, gbc_listSeleccionado);
 
 		JPanel panelAbajo = new JPanel();
 		GridBagConstraints gbc_panelAbajo = new GridBagConstraints();
@@ -227,6 +271,11 @@ public class PanelValoracionMateria extends JPanel {
 		panelAbajo.setLayout(gbl_panelAbajo);
 
 		JButton btnGuardarLasNotas = new JButton("Guardar las notas de todos los alumnos seleccionados");
+		btnGuardarLasNotas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				guardar();
+			}
+		});
 		GridBagConstraints gbc_btnGuardarLasNotas = new GridBagConstraints();
 		gbc_btnGuardarLasNotas.insets = new Insets(0, 0, 5, 0);
 		gbc_btnGuardarLasNotas.gridx = 0;
@@ -267,6 +316,183 @@ public class PanelValoracionMateria extends JPanel {
 		for (int i = 0; i <= 10; i++) {
 			this.comboBoxNota.addItem(i);
 		}
+	}
+
+	/**
+	 * 
+	 */
+	private void guardar() {
+		List<Estudiante> estudiantes = getEstudiantesSeleccionados();
+		
+		if (estudiantes != null) {
+			for (Estudiante estudiante : estudiantes) {
+				if (SuperControlador.obtenerNota(estudiante,
+						(Profesor) this.comboBoxProfesor.getSelectedItem(),
+						(Materia) this.comboBoxMateria.getSelectedItem()) == null) {
+					ControladorValoracionMateria.insert(estudiante, (Profesor) this.comboBoxProfesor.getSelectedItem(),
+							(Materia) this.comboBoxMateria.getSelectedItem(),
+							(Integer) this.comboBoxNota.getSelectedItem(), fecha);
+				} else {
+					ControladorValoracionMateria.update(estudiante, (Profesor) this.comboBoxProfesor.getSelectedItem(),
+							(Materia) this.comboBoxMateria.getSelectedItem(),
+							(Integer) this.comboBoxNota.getSelectedItem(), fecha);
+				}
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private List<Estudiante> getEstudiantesSeleccionados() {
+
+		List<Estudiante> l = new ArrayList<Estudiante>();
+		for (int i = 0; i < this.listMSeleccionados.size(); i++) {
+			Estudiante e = this.listMSeleccionados.get(i);
+			l.add(this.listMSeleccionados.get(i));
+		}
+
+		return l;
+	}
+
+	/**
+	 * 
+	 */
+	private void pasarASeleccionado() {
+		Estudiante selectedEstudiante = null;
+		int selectedIndex = listNoSeleccionados.getSelectedIndex();
+
+		if (selectedIndex != -1) {
+			selectedEstudiante = listMNoSeleccionados.getElementAt(selectedIndex);
+
+		}
+
+		listMSeleccionados.addElement(selectedEstudiante);
+		listMNoSeleccionados.remove(selectedIndex);
+
+	}
+
+	/**
+	 * 
+	 */
+	private void quitarDeSeleccionado() {
+		Estudiante selectedEstudiante = null;
+		int selectedIndex = listSeleccionados.getSelectedIndex();
+
+		if (selectedIndex != -1) {
+			selectedEstudiante = listMSeleccionados.getElementAt(selectedIndex);
+		}
+
+		listMNoSeleccionados.addElement(selectedEstudiante);
+		listMSeleccionados.remove(selectedIndex);
+	}
+
+	/**
+	 * 
+	 */
+	private void pasarTodosASeleccionados() {
+
+		this.listMNoSeleccionados.removeAllElements();
+		this.listMSeleccionados.removeAllElements();
+
+		List<Estudiante> estudiantes = (List<Estudiante>) ControladorEstudiante.getInstance().findAll();
+
+		for (Estudiante estudiante : estudiantes) {
+			this.listMSeleccionados.addElement(estudiante);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void quitarTodosDeSeleccionados() {
+
+		this.listMNoSeleccionados.removeAllElements();
+		this.listMSeleccionados.removeAllElements();
+
+		List<Estudiante> estudiantes = (List<Estudiante>) ControladorEstudiante.getInstance().findAll();
+
+		for (Estudiante estudiante : estudiantes) {
+			this.listMNoSeleccionados.addElement(estudiante);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void actualizarAlumnado() {
+		this.listMNoSeleccionados.removeAllElements();
+		this.listMSeleccionados.removeAllElements();
+
+		List<Estudiante> estudiantes = (List<Estudiante>) ControladorEstudiante.getInstance().findAll();
+
+		for (Estudiante estudiante : estudiantes) {
+			if (SuperControlador.ordenarEstudiantes(estudiante, (Profesor) this.comboBoxProfesor.getSelectedItem(),
+					(Materia) this.comboBoxMateria.getSelectedItem(),
+					(Integer) this.comboBoxNota.getSelectedItem()) != null) {
+				this.listMSeleccionados.addElement(estudiante);
+			} else {
+				this.listMNoSeleccionados.addElement(estudiante);
+
+			}
+		}
+
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public DefaultListModel<Estudiante> getListModelNOSeleccionados() {
+		if (this.listNoSeleccionados == null) {
+			this.listMNoSeleccionados = new DefaultListModel<Estudiante>();
+
+		}
+		return listMNoSeleccionados;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public DefaultListModel<Estudiante> getListModelSeleccionados() {
+		if (this.listMSeleccionados == null) {
+			this.listMSeleccionados = new DefaultListModel<Estudiante>();
+
+		}
+		return listMSeleccionados;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	private JFormattedTextField getJFormattedTextFieldDatePersonalizado() {
+		jftf = new JFormattedTextField(new JFormattedTextField.AbstractFormatter() {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+			@Override
+			public String valueToString(Object value) throws ParseException {
+				if (value != null && value instanceof Date) {
+					return sdf.format(((Date) value));
+				}
+				return "";
+			}
+
+			@Override
+			public Object stringToValue(String text) throws ParseException {
+				try {
+					return sdf.parse(text);
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Error en la fecha");
+					return null;
+				}
+			}
+		});
+		jftf.setColumns(20);
+		jftf.setValue(new Date());
+		return jftf;
 	}
 
 }
